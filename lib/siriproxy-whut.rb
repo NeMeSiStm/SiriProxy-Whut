958208fa48dcd9da1230f7fa471d7d916f7adb62
NeMeSiS_tm <nemesistm@gmail.com>
2013-02-19 17:27:30 +0100

    erweiterte funktion

--- lib/siriproxy-whut.rb
+++ lib/siriproxy-whut.rb
@@ -6,10 +6,38 @@ class SiriProxy::Plugin::Whut < SiriProxy::Plugin
     def initialize(config)
         #if you have custom configuration options, process them here!
     end
+
+listen_for /sonos erhöhe Lautstärke im Badezimmer/i do
+    open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=VolumeUp")
     
-    listen_for /test Musik/i do
+    say "Die Lautstärke im Badezimmer wurde erhöht."
+    
+    request_completed
+    #Die Lautstärke wird um einen Punkt erhöht.
+end
+
+listen_for /sonos veringere Lautstärke im Badezimmer/i do
+    open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=VolumeDown")
+    
+    say "Die Lautstärke im Badezimmer wurde verringert."
+    
+    request_completed
+    #Die Lautstärke wird um einen Punkt verringert.
+end
+
+listen_for /sonos starte die Wiedergabe im Badezimmer/i do
+    open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=Play")
+    
+    say "Die Musik im Badezimmer wurde gestartet."
+    
+    request_completed
+    #Musik im Badezimmer Ein.
+end
+
+listen_for /test Musik/i do
     say "Hallo Michael, Sonos zu PHP funktioniert einwandfrei. Glaube ich zumindest." #say something to the user!
     
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-    end
+end
+
 end
